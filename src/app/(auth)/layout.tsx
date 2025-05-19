279a97efbd14df521ad68ee4a12f38d95b2e1b84
Yuristian <yuristian.ardianto@gmail.com>
2025-05-19 21:44:24 +0700

    add auth page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" height={40} width={120} alt="Logo" />
+          <div className="flex items-center gap-2">
+            <Button asChild variant="secondary">
+              <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                {isSignIn ? "Sign-Up" : "Login"}
+              </Link>
+            </Button>
+          </div>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthLayout;
